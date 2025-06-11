fc1e45c11319c0ce236ca069c3af0e11739e505b
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-06-11 23:05:40 +0200

    FeedView UI

---
+++ ThreadsTutorials/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCell.swift
+//  ThreadsTutorials
+//
+//  Created by Roger Florat Gutierrez on 11/06/25.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
